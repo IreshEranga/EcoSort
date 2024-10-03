b047b738e3aeb85025c4c5c219830c14b1ae09e2
Iresh Eranga <iresheranga2020al@gmail.com>
2024-10-03 09:26:31 +0530

    Update app-routes.js

--- frontend/src/routes/app-routes.js
+++ frontend/src/routes/app-routes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from '../components/Home';
+import Login from '../pages/Login/Login';
 
 const AppRoutes = () => {
     return (
@@ -9,6 +10,7 @@ const AppRoutes = () => {
           <Router>
               <Routes>
                   <Route path="/" element={<Home />} />                  
+                  <Route path="/login" element={<Login />} />                  
               </Routes>
           </Router>
       </>
