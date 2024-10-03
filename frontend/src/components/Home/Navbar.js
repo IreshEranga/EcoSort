1b0ed62d0d3bdd942323795aee9957d7aa2ee0fb
Iresh Eranga <iresheranga2020al@gmail.com>
2024-10-03 09:26:57 +0530

    Update Navbar.js

--- frontend/src/components/Home/Navbar.js
+++ frontend/src/components/Home/Navbar.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Logo from '../../assets/ECOSORT.png';
 import './home.css';
 
 const NavigationBar = () => {
     return (
         <Navbar expand="lg" className="navbar-custom" fixed="top">
-            <Container className="d-flex justify-content-center">
-                <Navbar.Brand as={Link} to="/" className="navbar-logo mx-auto">
-                    <i className="fas fa-leaf"></i> Eco-Sort
+            <Container className="d-flex justify-content-between align-items-center">
+                <Navbar.Brand as={Link} to="/" className="navbar-logo">
+                    <Image className='logo' src={Logo} />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
-                    <Nav className="nav-items mx-auto">
+                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                    <Nav className="nav-items">
                         <Nav.Link as={Link} to="/" className="nav-link-custom">Home</Nav.Link>
                         <Nav.Link as={Link} to="/features" className="nav-link-custom">Features</Nav.Link>
                         <Nav.Link as={Link} to="/about" className="nav-link-custom">About</Nav.Link>
