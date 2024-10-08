f1a9629d3354ccbc7a5d007d35fb890b201c3267
Iresh Eranga <iresheranga2020al@gmail.com>
2024-10-08 14:37:20 +0530

    Create UsersPage.jsx

---
+++ frontend/src/components/Admin/UsersPage/UsersPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import AdminSidebar from '../../../components/Admin/AdminSidebar'
+function UsersPage() {
+  return (
+    <div>UsersPage</div>
+  )
+}
+
+export default UsersPage
