b04e018fb220724b22ea52851c86aaf5147a71db
Iresh Eranga <iresheranga2020al@gmail.com>
2024-10-10 00:06:35 +0530

    Create driverRoutes.js

---
+++ backend/routes/driverRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const driverController = require('../controllers/driverController');
+
+const router = express.Router();
+
+router.post('/drivers', driverController.createDriver);
+router.get('/drivers', driverController.getAllDrivers);
+router.get('/drivers/:id', driverController.getDriverById);
+router.put('/drivers/:id', driverController.updateDriver);
+router.delete('/drivers/:id', driverController.deleteDriver);
+router.get('/drivers/available/:city', driverController.getAvailableDriversByCity);
+
+module.exports = router;
