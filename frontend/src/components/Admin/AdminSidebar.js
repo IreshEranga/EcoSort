466f04d63cdeeb48136bcb8984fad4adee4f0e6a
Iresh Eranga <iresheranga2020al@gmail.com>
2024-10-11 15:07:15 +0530

    Update AdminSidebar.js

--- frontend/src/components/Admin/AdminSidebar.js
+++ frontend/src/components/Admin/AdminSidebar.js
@@ -25,10 +25,12 @@ export default function AdminSidebar() {
       <ul className="nav-links">
         <li><Link to="/admindashboard">Dashboard</Link></li>
         <li><Link to="/users">Users</Link></li>
+        <li><Link to="/admindashboard/drivers">Drivers</Link></li>
         <li><Link to="/waste-management">Waste Management</Link></li>
         <li><Link to="/admindashboard/collection-routine">Collection Routines</Link></li>
         <li><Link to="/waste-management">Payments</Link></li>
         <li><Link to="/waste-management">Issues</Link></li>
+  
         {/* Add more links as needed */}
       </ul>
 
