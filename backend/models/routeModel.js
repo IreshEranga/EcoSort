5d3442f92935c5ae9cbaf41ed6386bac9e2e4605
Iresh Eranga <iresheranga2020al@gmail.com>
2024-10-12 13:31:31 +0530

    Create routeModel.js

---
+++ backend/models/routeModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const RouteSchema = new mongoose.Schema({
+  routeName: {
+    type: String,
+    required: true,
+  },
+  routes: [
+    {
+      lat: { type: Number, required: true },
+      lng: { type: Number, required: true },
+    },
+  ],
+  assignedDriver: {
+    type: mongoose.Schema.Types.ObjectId, // Assuming you have a Driver model
+    ref: 'Driver'
+  },
+  date:{type : String},
+}, { timestamps: true });
+
+module.exports = mongoose.model('Route', RouteSchema);
