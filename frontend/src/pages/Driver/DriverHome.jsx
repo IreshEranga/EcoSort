a9ce2ab1aad47b02a1c62b861cf06f9e2c21d159
Iresh Eranga <iresheranga2020al@gmail.com>
2024-10-13 15:12:38 +0530

    Update DriverHome.jsx

--- frontend/src/pages/Driver/DriverHome.jsx
+++ frontend/src/pages/Driver/DriverHome.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import DriverNavBar from './DriverNavBar';
+import './DriverHome.css'; // Importing CSS for styling
+import collecrWasteDriver from '../../assets/images/collecrWasteDriver.jpeg'
+
+
+
 function DriverHome() {
   const navigate = useNavigate(); // Hook to navigate to different routes
 
-  // Handle logout
-
+  const Driver = JSON.parse(localStorage.getItem('driver'));
+  console.log(Driver.id);
 
   return (
-    <div>
-      <DriverNavBar/>
-      <h1>Driver Home</h1>
-      
+    <div className="driver-home">
+      <DriverNavBar />
+      <div className="drivercontainer">
+        <center><h1>Welcome, {Driver.name}</h1></center>
+        <center><p>Your journey to efficient waste management starts here.</p></center>
+        
+        <div className="info-section">
+          <img src={collecrWasteDriver} alt="Waste Management" className="home-image" />
+          <div className="details">
+            <h2>Your Responsibilities</h2>
+            <ul>
+              <li>Collect waste on scheduled days</li>
+              <li>Ensure proper disposal of waste</li>
+              <li>Maintain communication with the central office</li>
+            </ul>
+          </div>
+        </div>
+
+        
+      </div>
     </div>
   );
 }
