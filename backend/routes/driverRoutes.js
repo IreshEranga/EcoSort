faaef5fe308223c938d264db7d9127d6a9e38f8f
Iresh Eranga <iresheranga2020al@gmail.com>
2024-10-13 13:46:14 +0530

    Update driverRoutes.js

--- backend/routes/driverRoutes.js
+++ backend/routes/driverRoutes.js
@@ -10,5 +10,6 @@ router.get('/drivers/:id', driverController.getDriverById);
 router.put('/drivers/:id', driverController.updateDriver);
 router.delete('/drivers/:id', driverController.deleteDriver);
 router.get('/drivers/available/:city', driverController.getAvailableDriversByCity);
+router.post('/login', driverController.driverLogin);
 
 module.exports = router;
