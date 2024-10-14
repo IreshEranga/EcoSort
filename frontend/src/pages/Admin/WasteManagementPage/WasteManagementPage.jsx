fb5322ae885a75c93b197502fcf0f2e137cbb415
Iresh Eranga <iresheranga2020al@gmail.com>
2024-10-14 13:07:45 +0530

    kv update

--- frontend/src/pages/Admin/WasteManagementPage/WasteManagementPage.jsx
+++ frontend/src/pages/Admin/WasteManagementPage/WasteManagementPage.jsx
@@ -29,27 +29,81 @@ function WasteManagementPage() {
             <tr>
               <th>User ID</th>
               <th>Name</th>
-              <th>Bin ID</th>
-              <th>Type</th>
-              <th>Percentage</th>
-              <th>QR Code</th>
+              <th>Bin ID (Organic)</th>
+              <th>Percentage (Organic)</th>
+              <th>QR Code (Organic)</th>
+              <th>Bin ID (Paper)</th>
+              <th>Percentage (Paper)</th>
+              <th>QR Code (Paper)</th>
+              <th>Bin ID (Plastic)</th>
+              <th>Percentage (Plastic)</th>
+              <th>QR Code (Plastic)</th>
+              <th>Bin ID (Electric)</th>
+              <th>Percentage (Electric)</th>
+              <th>QR Code (Electric)</th>
+              <th>Bin ID (Other)</th>
+              <th>Percentage (Other)</th>
+              <th>QR Code (Other)</th>
             </tr>
           </thead>
           <tbody>
-            {binsData.map(user => 
-              user.bins.map(bin => (
-                <tr key={bin.binId}>
+            {binsData.map(user => {
+              // Initialize bins details
+              const binsMap = {
+                Organic: { binId: '', percentage: '', qr: '' },
+                Paper: { binId: '', percentage: '', qr: '' },
+                Plastic: { binId: '', percentage: '', qr: '' },
+                Electric: { binId: '', percentage: '', qr: '' },
+                Other: { binId: '', percentage: '', qr: '' },
+              };
+
+              // Fill the binsMap based on user's bins
+              user.bins.forEach(bin => {
+                binsMap[bin.type] = { binId: bin.binId, percentage: bin.percentage, qr: bin.qr };
+              });
+
+              return (
+                <tr key={user.userId}>
                   <td>{user.userId}</td>
                   <td>{user.name}</td>
-                  <td>{bin.binId}</td>
-                  <td>{bin.type}</td>
-                  <td>{bin.percentage}%</td>
+                  <td>{binsMap.Organic.binId}</td>
+                  <td>{binsMap.Organic.percentage}%</td>
                   <td>
-                    <img src={bin.qr} alt={`QR code for bin ${bin.binId}`} style={{ width: '50px', height: '50px' }} />
+                    {binsMap.Organic.qr && (
+                      <img src={binsMap.Organic.qr} alt={`QR code for bin ${binsMap.Organic.binId}`} style={{ width: '50px', height: '50px' }} />
+                    )}
+                  </td>
+                  <td>{binsMap.Paper.binId}</td>
+                  <td>{binsMap.Paper.percentage}%</td>
+                  <td>
+                    {binsMap.Paper.qr && (
+                      <img src={binsMap.Paper.qr} alt={`QR code for bin ${binsMap.Paper.binId}`} style={{ width: '50px', height: '50px' }} />
+                    )}
+                  </td>
+                  <td>{binsMap.Plastic.binId}</td>
+                  <td>{binsMap.Plastic.percentage}%</td>
+                  <td>
+                    {binsMap.Plastic.qr && (
+                      <img src={binsMap.Plastic.qr} alt={`QR code for bin ${binsMap.Plastic.binId}`} style={{ width: '50px', height: '50px' }} />
+                    )}
+                  </td>
+                  <td>{binsMap.Electric.binId}</td>
+                  <td>{binsMap.Electric.percentage}%</td>
+                  <td>
+                    {binsMap.Electric.qr && (
+                      <img src={binsMap.Electric.qr} alt={`QR code for bin ${binsMap.Electric.binId}`} style={{ width: '50px', height: '50px' }} />
+                    )}
+                  </td>
+                  <td>{binsMap.Other.binId}</td>
+                  <td>{binsMap.Other.percentage}%</td>
+                  <td>
+                    {binsMap.Other.qr && (
+                      <img src={binsMap.Other.qr} alt={`QR code for bin ${binsMap.Other.binId}`} style={{ width: '50px', height: '50px' }} />
+                    )}
                   </td>
                 </tr>
-              ))
-            )}
+              );
+            })}
           </tbody>
         </table>
       </div>
